48e947f34eaa38838509b6b8984dca87907f964f
Umalat <dukuev037@mail.ru>
2023-03-28 16:10:35 +0300

    Modified found.cpp

--- HW1/src/found.cpp
+++ HW1/src/found.cpp
@@ -4,29 +4,27 @@
 #include <string>
 #include <vector>
 
-std::vector<std::string> split(std::string str, char sep){
+std::vector<std::string> Split(std::string str, char sep){
 	std::stringstream ss(str);
-	std::vector<std::string> res(10, "");
 	std::string temporary;
 	std::vector<std::string> result;
 	while (std::getline(ss, temporary, sep)){
 		result.push_back(temporary);
 	}
-	if (result.size()>0){
-		return result;}
-	else{
-		return res;
-	}
+	return result;
 }
 
-std::string func(std::ifstream &file, std::string name){
+
+std::string FoundTheDate(std::istream &file, std::string name){
 	while (file){
 		std::string str;
 		getline(file, str, '\n');
 		std::vector<std::string> vect;
-		vect = split(str, '\t');
-  		if (vect[2] == name){
-  			return vect[5];
+		if (str != ""){
+			vect = Split(str, '\t');
+  			if (vect[2] == name){
+  				return vect[5];
+			}
 		} 
 	}
 	return "";
@@ -34,7 +32,6 @@ std::string func(std::ifstream &file, std::string name){
 
 
 int main(int argc, char *argv[]){ 
-	//std::ifstream file("artist_alias");
 	std::string artist_alias_path;
 	if (argc != 4){
 		std::cout << "Invalid number of arguments!" << std::endl;
@@ -46,22 +43,22 @@ int main(int argc, char *argv[]){
 		if (arg == "--artist_alias_path")
 			artist_alias_path = std::string(argv[i+1]);
 	}
+	
 	std::ifstream file(artist_alias_path);
+	if (!file){
+		std::cout << "Something went wrong while opening file" << std::endl;
+		return -1;
+	}
+	
 	std::string name = argv[1]; 
-	if (file){	
-		std::string date = func(file, name);
-		if (date != ""){
-			std::cout <<"Дата создания: " << date << std::endl;
-		}
-		else{
-			std::cout << "Name not found!" << std::endl;
-		}
+	std::string date = FoundTheDate(file, name);
+	if (date != ""){
+		std::cout <<"Дата создания: " << date << std::endl;
 	}
 	else{
-		std::cout << "No such file as" << std::endl;
+		std::cout << "Name not found!" << std::endl;
 	}
 	
 	file.close();
 	return 0;
 }
-
